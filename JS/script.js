2ecdfc72199ec487e0d09ca24069e5236e59768c
Abdelrahman-Adel610 <engba80818233@gmail.com>
2024-08-19 12:41:58 +0300

    Changing the current score by dice changing + make hold btn to change turns between p1 & p2

--- JS/script.js
+++ JS/script.js
@@ -9,6 +9,16 @@ let newGame = btns[0];
 let rollDice = btns[1];
 let hold = btns[2];
 let diceImage = document.querySelector(".btns img");
+let player1 = document.querySelector(".p1 ");
+let player2 = document.querySelector(".p2 ");
+let cScore1 = document.querySelector(".p1 .crnt-score");
+let cScore2 = document.querySelector(".p2 .crnt-score");
+/**********************VARIABLES**********************/
+let currentScore1 = 0;
+let currentScore2 = 0;
+let state = 0;
+// 0 ==>player 1 is currently playing
+// 1 ==>player 2 is currently playing
 
 /**********************UTILITIES**********************/
 function removeModal() {
@@ -38,4 +48,17 @@ rollDice.addEventListener("click", function () {
   let randomNum = Math.trunc(Math.random() * 6) + 1;
   diceImage.classList.remove("hidden");
   diceImage.setAttribute("src", `Images/dice-${randomNum}.png`);
+  if (!state) {
+    currentScore1 += randomNum;
+    cScore1.textContent = currentScore1;
+  } else {
+    currentScore2 += randomNum;
+    cScore2.textContent = currentScore2;
+  }
+});
+
+hold.addEventListener("click", function () {
+  state = !state;
+  player1.classList.toggle("focus");
+  player2.classList.toggle("focus");
 });
