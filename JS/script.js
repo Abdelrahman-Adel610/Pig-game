ae69aeb8ff8125a0dbef63481c1144f4f844ea5a
Abdelrahman-Adel610 <engba80818233@gmail.com>
2024-08-19 12:03:02 +0300

    finished modal window functionality (openning & closing)

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,27 @@
 "use strict";
+/**********************ELEMENTS**********************/
+let info = document.querySelector(".info");
+let layout = document.querySelector(".layout");
+let modal = document.querySelector(".modal");
+let closeModal = document.querySelector(".modal i");
+/**********************UTILITIES**********************/
+function removeModal() {
+  layout.classList.add("hidden");
+  modal.classList.add("hidden");
+}
+/**********************EVENTS**********************/
+info.addEventListener("click", function () {
+  layout.classList.remove("hidden");
+  modal.classList.remove("hidden");
+});
+closeModal.addEventListener("click", function () {
+  removeModal();
+});
+layout.addEventListener("click", function () {
+  removeModal();
+});
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    removeModal();
+  }
+});
